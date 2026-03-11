1b57457e41d4183f2128ff10ef37559876fd5835
manasjana <mjana1164@gmail.com>
2026-03-11 22:11:29 +0530

    group action computation

--- group_action.py
+++ group_action.py
@@ -1,3 +1,40 @@
+##########################################################################################################
+#                                                                                                        
+# This  code was obtained by modifying that of SiGamal available online at :                             
+# https://github.com/BorisFouotsa/SimS/                                                                  
+#                                                                                                        
+#                                                                                                        
+################################################################################
+# Some functions in this file are based on those in SIDH Library.
+# 
+# SIDH Library: https://www.microsoft.com/en-us/research/project/sidh-library/
+# SIDH Library
+# 
+# Copyright (c) Microsoft Corporation
+# All rights reserved. 
+# 
+# MIT License
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+# associated documentation files (the ""Software""), to deal in the Software without restriction, 
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial 
+# portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+# LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+#############################################################################################################
+
+
+
+
 import random
 from utils import *
 from montgomery import *
